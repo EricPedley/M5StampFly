47d024a8d1a7a4b99827bc1da1356ee2a1678cab
Eric Pedley <ericpedley@gmail.com>
2025-10-26 07:50:44 -0700

    add shit to prevent c name mangling

--- src/neural_network.h
+++ src/neural_network.h
@@ -1,6 +1,14 @@
 #ifndef NEURAL_NETWORK_H
 #define NEURAL_NETWORK_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void nn_forward(const float* input, float* output);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // NEURAL_NETWORK_H
