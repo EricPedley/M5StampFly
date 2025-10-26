8e1a87133a62b7ec35786690ed40a6cbe6aa9b29
Eric Pedley <ericpedley@gmail.com>
2025-10-26 07:16:07 -0700

    add code to nn controller, still incomplete though

--- src/nn_control.cpp
+++ src/nn_control.cpp
@@ -1,3 +1,85 @@
+#include "neural_network.h"
+#include "sensor.hpp"
+
+void vector_add(const float* a, const float* b, float* out, int size) {
+    for (int i = 0; i < size; i++) {
+        out[i] = a[i] + b[i];
+    }
+}
+
+void matmul_vector(const float* mat, const float* vec, float* out, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        out[i] = 0.0f;
+        for (int j = 0; j < cols; j++) {
+            out[i] += mat[i * cols + j] * vec[j];
+        }
+    }
+}
+
 void nn_control(void) {
+    float nn_input[12];
+    float nn_output[4];
+
+    // TODO: euler to axis-angle representation
+
+    // p_rate = Roll_rate;
+    // q_rate = Pitch_rate;
+    // r_rate = Yaw_rate;
+
+    // TODO: convert euler angles to projected gravity vector, and linear velocity + desired position to body frame
+    float roll_vector[3] = {Roll_angle, 0.0f, 0.0f};
+    float pitch_vector[3] = {0.0f, Pitch_angle, 0.0f};
+    float yaw_vector[3] = {0.0f, 0.0f, Yaw_angle};
+    float ang_vel[3];
+    vector_add(roll_vector, pitch_vector, ang_vel, 3);
+    vector_add(ang_vel, yaw_vector, ang_vel, 3);
+    float roll_matrix[9] = {
+        1, 0, 0,
+        0, cosf(Roll_angle), -sinf(Roll_angle),
+        0, sinf(Roll_angle), cosf(Roll_angle)
+    };
+    float pitch_matrix[9] = {
+        cosf(Pitch_angle), 0, sinf(Pitch_angle),
+        0, 1, 0,
+        -sinf(Pitch_angle), 0, cosf(Pitch_angle)
+    };
+    float yaw_matrix[9] = {
+        cosf(Yaw_angle), -sinf(Yaw_angle), 0,
+        sinf(Yaw_angle), cosf(Yaw_angle), 0,
+        0, 0, 1
+    };
+    float projected_gravity[3] = {0.0f, 0.0f, -9.81f};
+    matmul_vector(roll_matrix, projected_gravity, projected_gravity, 3, 3);
+    matmul_vector(pitch_matrix, projected_gravity, projected_gravity, 3, 3);
+    matmul_vector(yaw_matrix, projected_gravity, projected_gravity, 3, 3);
+
+    // Roll_angle  ;
+    // Pitch_angle ;
+    // Yaw_angle   ;
+
+
+    // self._robot.data.root_lin_vel_b,
+    // self._robot.data.root_ang_vel_b,
+    // self._robot.data.projected_gravity_b,
+    // desired_pos_b,
+
+    nn_forward(nn_input, nn_output);
+
+
+        if (OverG_flag == 0) {
+            set_duty_fr(FrontRight_motor_duty);
+            set_duty_fl(FrontLeft_motor_duty);
+            set_duty_rr(RearRight_motor_duty);
+            set_duty_rl(RearLeft_motor_duty);
+        } else {
+            FrontRight_motor_duty = 0.0;
+            FrontLeft_motor_duty  = 0.0;
+            RearRight_motor_duty  = 0.0;
+            RearLeft_motor_duty   = 0.0;
+            motor_stop();
+            // OverG_flag=0;
+            Mode = PARKING_MODE;
+        }
+    }
 
 }
